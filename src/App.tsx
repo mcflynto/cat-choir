631e63cbec90a1373bb27862f417288f58c1d8d9
Julian Treutler <julian.treutler@gmail.com>
2021-04-24 14:53:38 +0200

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const removeTransition = (e: any) => {
+    if (e.propertyName !== "transform") return;
+    e.target.classList.remove("playing");
+  };
+
+  const playSound = (e: KeyboardEvent) => {
+    const audio: HTMLAudioElement | null = document.querySelector(
+      `audio[data-key="${e.key}"]`
+    );
+    const cat = document.querySelector(`div[data-key="${e.key}"]`);
+
+    if (!audio || !cat) return;
+
+    cat.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  const keys = ["a", "s", "d"];
+
+  const cats = Array.from(document.querySelectorAll(".cat"));
+  cats.forEach((cat) =>
+    cat.addEventListener("transitionend", (e) => removeTransition(e))
+  );
+  window.addEventListener("keydown", (e) => playSound(e));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cat Choir</h1>
+
+      {keys.map((n) => (
+        <div className="cat" data-key={n} key={n}>
+          {n}
+        </div>
+      ))}
+
+      <audio data-key="a" src="sounds/cats.mp3"></audio>
+      <audio data-key="s" src="sounds/cats.mp3"></audio>
+      <audio data-key="d" src="sounds/cats.mp3"></audio>
     </div>
   );
-}
+};
 
 export default App;
